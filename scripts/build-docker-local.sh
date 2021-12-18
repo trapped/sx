e186cb387d39289f2896d1d9512d55281b2fa77c
Giorgio Pellero <giorgio.pellero@gmail.com>
2021-12-18 17:29:45 +0100

    feat: Kubernetes manifests, CI integration tests, release workflow

---
+++ scripts/build-docker-local.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Builds a local Docker image for testing in kind.
+
+docker build -t ghcr.io/trapped/sx:latest .
---
+++ scripts/test-kind.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# This script is used for testing configuration autoreload with Kubernetes
+# ConfigMaps:
+#
+# 1. spins up a kind (Kubernetes in Docker) cluster
+# 2. deploys SX using kubectl and the default manifests in kubernetes/
+# 3. checks SX logs for the word "listen"
+# 4. applies a new ConfigMap
+# 5. waits a few seconds, then checks SX logs searching for the word "reload"
+
+set -euxo pipefail
+
+# clean workspace just in case
+kind delete cluster
+
+# create Kubernetes cluster; automatically configures kubectl
+kind create cluster --config kind-config.yml
+
+# load latest image into kind
+kind load docker-image ghcr.io/trapped/sx:latest
+
+# apply manifests
+kubectl apply -f kubernetes/namespace.yml
+kubectl apply -f kubernetes/configmap-facts.yml
+kubectl apply -f kubernetes/service.yml
+kubectl apply -f kubernetes/deployment-neverpull.yml
+
+# wait for SX to start
+kubectl rollout status --namespace=sx deploy/sx --timeout=60s
+
+# check SX is up
+if ! kubectl logs --namespace=sx deploy/sx | grep "listen"; then
+    echo "ERROR: SX does not appear up yet"
+    exit 1
+fi
+
+# apply a new config
+kubectl apply -f kubernetes/configmap-catpics.yml
+
+# check SX reloaded its config
+if ! timeout 90s grep -m1 reload <(kubectl logs -f --namespace=sx deploy/sx | tee /dev/stderr); then
+    echo "ERROR: SX has not reloaded its config yet"
+    exit 1
+fi
