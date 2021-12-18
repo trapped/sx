4fbab88a16255c9ad2a18ff864783ca86536bbdb
Giorgio Pellero <giorgio.pellero@gmail.com>
2021-12-18 17:40:07 +0100

    chore: increase integration test timeouts to 5m

--- scripts/test-kind.sh
+++ scripts/test-kind.sh
@@ -27,7 +27,7 @@ kubectl apply -f kubernetes/service.yml
 kubectl apply -f kubernetes/deployment-neverpull.yml
 
 # wait for SX to start
-kubectl rollout status --namespace=sx deploy/sx --timeout=60s
+kubectl rollout status --namespace=sx deploy/sx --timeout=300s
 
 # check SX is up
 if ! kubectl logs --namespace=sx deploy/sx | grep "listen"; then
@@ -39,7 +39,7 @@ fi
 kubectl apply -f kubernetes/configmap-catpics.yml
 
 # check SX reloaded its config
-if ! timeout 90s grep -m1 reload <(kubectl logs -f --namespace=sx deploy/sx | tee /dev/stderr); then
+if ! timeout 300s grep -m1 reload <(kubectl logs -f --namespace=sx deploy/sx | tee /dev/stderr); then
     echo "ERROR: SX has not reloaded its config yet"
     exit 1
 fi
